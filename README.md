c9fb3556174ae0ea74c83a7f72cf908014365c76
Ali Firas <ali.albdaer.py@gmail.com>
2024-04-08 19:05:04 +0300

    Added config.py to .gitignore Modified README.md Added 2 executables in the new directory "apps" Renamed the server directory to scripts

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-This is NOT Enigma.
+# Async Socket Chat
 
-Async socket chat server and client scripts 
+## Description
+An async socket chat server.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+- There is no need to download any libraries, only Python +3.10 and asyncio are required.
+
+## Usage
+- Directly download apps/server.exe and apps/client.exe, provide a proper IP and Port (ex: "localhost", 8000) OR
+- Put your IP and Port in scripts/config.py and manually run scripts/server.py and scripts/client.py
+
+## License
+GNU General Public License
---
+++ scripts/client.py
@@ -0,0 +1,66 @@
+import asyncio
+
+from config import HOST, PORT
+from utils import print_color
+
+
+CONNECTED = False
+
+
+async def send_message(writer):
+    while CONNECTED:
+        message = await loop.run_in_executor(None, input, "\r")
+        print("\033[A\033[K", end="")  # Clear the input line
+        
+        try:
+            writer.write(message.encode())
+            await writer.drain()
+        except Exception as e:
+            print_color("red", e)
+            break
+
+        await asyncio.sleep(0.1)
+
+async def receive_messages(reader):
+    global CONNECTED
+
+    while CONNECTED:
+        data = await reader.read(1024)
+        if not data:
+            print_color("red", "Connection closed by server.")
+            CONNECTED = False
+        
+        print(data.decode())
+        await asyncio.sleep(0.1)
+
+
+async def handle_connection():
+    global CONNECTED
+    reader, writer = await asyncio.open_connection(HOST, PORT)
+    CONNECTED = True
+
+    welcome_message = await reader.read(1024)
+    print(welcome_message.decode())
+
+    # Tasks for sending and receiving messages concurrently
+    send_task = asyncio.create_task(send_message(writer))
+    receive_task = asyncio.create_task(receive_messages(reader))
+
+    await asyncio.gather(send_task, receive_task)  # asyncio.gather for concurrent waiting
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    global loop  # global loop for send_message
+    loop = asyncio.get_event_loop()
+    await handle_connection()
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+
+    except KeyboardInterrupt:
+        print_color("red", "Disconnected from server.")
